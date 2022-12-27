97a0bb4a86f86c8c9a59a5560d87eec5252a0993
haribadireddy <murthyhari11@gmail.com>
2022-12-27 23:25:11 +0530

    added second max function and cleaned the file

--- quiz/2ndmaxno.py
+++ quiz/2ndmaxno.py
@@ -1,12 +1,15 @@
-lst=[1,3,4,6,2,70,90,77,65,54,103]
-max1=lst[0]
-max2=lst[0]
-for i in lst:
-    if i>max1:
-        max1=i
-for i in lst:
-    if max1>i>max2:
-        max2=i
+def second_max(lst):
+    max1 = lst[0]
+    max2 = lst[0]
+    for i in lst:
+        if i > max1:
+            max1 = i
+    for j in lst:
+        if max1 > j > max2:
+            max2 = j
+    return max1, max2
 
-print(max1)
-print(max2)
+
+lst1 = [1, 3, 4, 6, 2, 70, 90, 90, 77, 65, 54, 103]
+print(second_max(lst1))
+print(second_max([20, 40, 2, 30, 1, 40]))
