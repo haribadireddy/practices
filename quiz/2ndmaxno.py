b9d2c29925a78acf5b3f519354d78de948fe2821
haribadireddy <murthyhari11@gmail.com>
2022-12-28 00:12:06 +0530

    optimised second max in list

--- quiz/2ndmaxno.py
+++ quiz/2ndmaxno.py
@@ -10,6 +10,20 @@ def second_max(lst):
     return max1, max2
 
 
+def second_max2(lst):
+    max1 = -1000
+    max2 = -1000
+    for i in lst:
+        if i > max1:
+            max2 = max1
+            max1 = i
+        elif i > max2 and i != max1:
+            max2 = i
+
+    return max1, max2
+
+
 lst1 = [1, 3, 4, 6, 2, 70, 90, 90, 77, 65, 54, 103]
-print(second_max(lst1))
-print(second_max([20, 40, 2, 30, 1, 40]))
+lst2 = [1, 3, 2, 4]
+print(second_max2(lst2))
+print(second_max2([20, 40, 2, 30, 1, 40]))
