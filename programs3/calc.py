09e4558694e0f46abbaa13ff660f277e2207ebdd
haribadireddy <murthyhari11@gmail.com>
2022-12-28 22:43:35 +0530

    using functions from different module by importing

---
+++ programs3/calc.py
@@ -0,0 +1,9 @@
+from modules import *
+a=9
+b=3
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+print(square(a))
+print(cube(a))
---
+++ programs3/modules.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a*b
+
+
+def div(a,b):
+    return a/b
+
+
+def square(a):
+    return a**2
+
+
+def cube(a):
+    return a**3
