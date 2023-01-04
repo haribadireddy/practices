e1909749952dae678c7c3f0110b2587b842558c1
haribadireddy <murthyhari11@gmail.com>
2023-01-04 23:02:22 +0530

    avg of marks of a students

--- hacker rank/find the percentage.py
+++ hacker rank/find the percentage.py
@@ -1,12 +1,27 @@
-n=int(input("enter the no of students: "))
-students={}
+n = int(input())
+students = {}
 for i in range(n):
-    name=input("enter the name:")
-    for j in range(3):
-        marks=input("enter the marks")
-        students[name]=marks
+    line = input().split(" ")
+    name = line[0]
+    marks = list(map(float, line[1:4]))
+    students[name] = marks
+query_name = input()
+print(students)
+
+print(students[query_name])
+add = sum(students[query_name])
+result = add / len(students[query_name])
+print("%.2f" % result)
 
 print(students)
 
-
-
+"""n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+add=sum(student_marks[query_name])
+result=add/len(student_marks[query_name])
+print("%.2f" % result)"""
